6df42ef7ce9a5f731acafbaefe035f3dbc32b382
Maple.Wu <lingfeng.wu@mihoyo.com>
2023-10-10 15:03:10 +0800

    feat(copyright): update

--- zcore/generate.go
+++ zcore/generate.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2023 Maple Wu <justmaplewu@gmail.com>
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package zcore
 
 import (
@@ -10,7 +27,8 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/Just-maple/gozz/internal/strcase"
+	"github.com/stoewer/go-strcase"
+
 	"github.com/Just-maple/gozz/internal/ztore"
 	"github.com/Just-maple/gozz/zutils"
 )
@@ -22,7 +40,7 @@ var (
 		"lower": strings.ToLower,
 		"upper": strings.ToUpper,
 		"snake": strcase.SnakeCase,
-		"camel": strcase.CamelCase,
+		"camel": strcase.LowerCamelCase,
 		"kebab": strcase.KebabCase,
 	}
 
